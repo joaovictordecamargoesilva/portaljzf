75a7bd21a2b95eb021f495cedcca70c25d701318
joaovictordecamargoesilva <joaovictor.piloto110@gmail.com>
2025-10-29 09:58:43 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -102,6 +102,7 @@ async function startServer() {
     };
 
     // Core Middlewares
+    app.set('trust proxy', 1); // Trust the first proxy
     app.use(cors({ origin: true, credentials: true }));
     app.use(express.json({ limit: '20mb' }));
     app.use(express.urlencoded({ extended: true, limit: '20mb' }));
